994272b7b58e296e257c5a26a0770863590189a8
HaviLee <Havi.X.Li.-ND@disney.com>
2018-12-03 17:12:51 +0800

    add my resume

---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,51 @@
+module.exports = {
+    experience_type: {
+        "internship": ""
+    },
+    skill_type: {
+        "frontend": "iOS开发",
+        "backend": "其他技术栈",
+        "miscellaneous": "其他"
+    },
+    education_type: {
+        "undergraduate": "（本科）",
+        "graduate":" (硕士) "
+    },
+    link_type: {
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣读书",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "语言水平",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "项目与工作经验",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "技能",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "链接"
+    },
+    project_type: {
+        "personal": "个人项目",
+        "disney":"埃森哲有限公司 -- 2017.04~至今",
+        "hengqi" : "上海恒企科技有限公司 -- 2014.12~2017.03",
+        "aolan" : "上海傲蓝科技有限公司 -- 2013.3~2014.10",
+        "team": "创业项目",
+        "work": "外包项目",
+    }
+};
